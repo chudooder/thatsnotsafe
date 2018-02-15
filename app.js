5d6618dfbc9048283de53c5536c4360d3aa6126d
chudooder <codeshawnachu@gmail.com>
2018-02-15 16:23:24 -0600

    add basic display

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 "use strict";
 
+Vue.component("frame-indicator", {
+    props: {
+        frameType: Number
+    },
+    methods: {
+        toColor: function(frameType) {
+            switch(frameType) {
+                case Rules.FrameType.NEUTRAL:
+                    return "#ffffff";
+                case Rules.FrameType.ATTACK_STARTUP:
+                    return "#ffd70f"
+                case Rules.FrameType.ATTACK_ACTIVE:
+                    return "#c63e00";
+                case Rules.FrameType.ATTACK_ACTIVE_NO_HITBOX:
+                    return "#ea6417";
+                case Rules.FrameType.ATTACK_RECOVERY:
+                    return "#9b044b";
+                case Rules.FrameType.HITSTUN:
+                    return "#ff1c1c";
+                case Rules.FrameType.BLOCKSTUN:
+                    return "#275fea";
+            }
+        }
+    },
+    template: '<div class="frame-indicator" :style="{backgroundColor: toColor(frameType)}"></div>'
+
+});
+
 var app = new Vue({
     el: "#app",
     data: {
         characters: ["Elphelt", "Elphelt"],
-        actions: {
-            0: ["5P"],
-            1: [null]
+        actions: [["5P"], ["_B"]],
+        frameData: [[],[]],
+    },
+
+    methods: {
+        calculateFrameData: function() {
+            this.frameData = Rules.calculateFrames(this.characters, this.actions);
         }
+    },
+
+    beforeMount(){
+        this.calculateFrameData();
     }
 });
